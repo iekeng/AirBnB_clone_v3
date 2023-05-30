f1f7914f6cbde586025bc1c5e0115df719f3b9b4
iekeng <ekeng7.eb@gmail.com>
2023-05-30 12:53:36 +0000

    api update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-"""Starting the API"""
+"""The api's application module"""
 
-from flask import Flask < F11 >
-from models import storage
 from api.v1.views import app_views
-from os import getenv
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from models import storage
+import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, origins=["0.0.0.0"])
+
+
+@app.errorhandler(404)
+def not_found(exception):
+    """Handles 404 errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.teardown_appcontext
-def close_storage(error):
-    """close storage"""
+def close_storage(exception):
+    """Method to handle teardown context"""
     storage.close()
 
 
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', 0.0.0.0)
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    host = os.getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = os.getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True, debug=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-from flask import Blueprint
-from api.v1.views.index import *
+"""Init file for package views"""
 
-app_views = Blueprint('api_views', __name__, url_prefix='/api/v1')
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+app_views = Blueprint('app_views', __name__, template_folder='templates')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.users import *
+from api.v1.views.amenities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
